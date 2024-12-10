24068bc9ceb8c413b329e660ff4a3c8579f54344
mwongess <amosmwongelah@gmail.com>
2024-12-11 00:27:40 +0300

    initial commit

---
+++ src/main/java/com/example/looker_embed/LookerEmbedApplication.java
@@ -0,0 +1,13 @@
+package com.example.looker_embed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookerEmbedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookerEmbedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/looker_embed/LookerEmbedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.looker_embed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookerEmbedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
